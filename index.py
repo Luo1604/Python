5ab3abf4359383cf7f8af9a0572473afbfd48603
Luo1604 <98147788+Luo1604@users.noreply.github.com>
2022-12-04 15:27:36 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,25 +1,31 @@
+from pyodide import JsException
 from pyodide.http import pyfetch
-import asyncio
-from bs4 import BeautifulSoup
 
-async def get_testdata(path) :
-    path = "https://www.andreasjakl.com/raycast-anchor-placing-ar-foundation-holograms-part-3/"
-    res = await pyfetch(url=path, method="GET")
-    obj = BeautifulSoup(await res.string(), 'html.parser')
-    list_title = []
-    for i in range(1, 7):
-        h = obj.find_all('h' + str(i))
-        if (h == []):
-            break
-        list_title.append(h) 
-    content = ""
-    for item in list_title:
-        for i in item:
-            title_text = i.get_text()
-            content += title_text
-    list_text = obj.find_all('p')
-    for item in list_text:
-        text = item.get_text()
-        if (len(text) > 50):
-            content += text
-    return content
+async def wiki_summary(slug):
+
+    try:
+        response = await pyfetch(
+            url=f"https://en.wikipedia.org/api/rest_v1/page/summary/{slug}",
+            method='GET',
+            headers={'Content-Type': 'application/json'}
+        )
+        if response.ok:
+            data = await response.json()
+            return data
+    except JsException:
+        return None
+
+async def get_summary(slug):
+    summary = await wiki_summary(slug)
+    if summary is None:
+        return f"""
+            <h1>No results found 😕.</h1>
+            <p>Wikipedia doesn't have an entry for {slug}. Either that, or Summer is confused. </p>
+            """
+    else:
+        output = f"""
+            <h1>{summary['title']}</h1>
+            <p>{summary['extract_html']}</p>
+            """
+
+        return output
